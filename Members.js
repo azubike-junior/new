752587075b6879dfa0eccaf27d0595045d4277da
zubi <azu@gmail.com>
2019-09-10 15:47:48 +0100

    changes

---
+++ Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id:1,
+        name: 'john Doe',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 2,
+        name: 'james Doe',
+        email: 'james@gmail.com',
+        status: 'active'
+    }, 
+    {
+        id: 3,
+        name: 'jude Doe',
+        email: 'jude@gmail.com',
+        status: 'inactive'
+    } 
+]
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
+
+const app = express();
+
+//init middleware 
+// app.use(logger);
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res)=> res.render('index', {
+    title:'Member App',
+    members
+}));
+
+//Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// set static folder; 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//member api/routes
+app.use('/api/members', require('./routes/api/members'));
+ 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
